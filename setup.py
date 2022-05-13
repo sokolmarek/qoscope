aaff607bbbe0de0513aefcd064db5fef4e396b86
Marek Sokol <12970094+sokolmarek@users.noreply.github.com>
2022-05-13 23:01:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/sokolmarek/qoscope",
+    include_package_data=True,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
